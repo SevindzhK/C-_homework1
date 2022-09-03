b93d86331bd62dd0b2cf525bb48d293656df18d3
SevindzhK <sevinge0709@gmail.com>
2022-09-03 17:21:10 +0300

    Added exercise_3

---
+++ Ex3/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным
+
+Console.WriteLine("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Number is even"); 
+}
+else 
+{
+    Console.WriteLine("Number isn't even");
+}
