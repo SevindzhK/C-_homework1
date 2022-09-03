0401a83f614da81c22def89ee8ccb2522a138add
SevindzhK <sevinge0709@gmail.com>
2022-09-03 17:49:22 +0300

    Added exercise_4

---
+++ Ex4/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("Enter natural number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int currentNumber = 1;
+
+while (currentNumber <= number)
+{
+    if (currentNumber % 2 == 0)
+    {
+        Console.WriteLine(currentNumber); 
+    }
+    currentNumber++;
+}    
