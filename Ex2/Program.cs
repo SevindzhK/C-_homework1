9e3f5d7544247b7f6f0da32939d1867e910b34e8
SevindzhK <sevinge0709@gmail.com>
2022-09-03 17:20:40 +0300

    Added exercise_2

---
+++ Ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Программа, которая принимает на вход три числа и выдаёт максимальное из этих чисел
+
+Console.WriteLine("Enter first number");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter second number");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Enter third number");
+int thirdNumber = Convert.ToInt32(Console.ReadLine());
+int max = firstNumber;
+if (secondNumber > max) max = secondNumber; 
+if (thirdNumber > max) max = thirdNumber; 
+
+Console.Write("max = ");
+Console.WriteLine(max);
